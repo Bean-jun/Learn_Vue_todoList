9f5f2ed8bd872cd05dfc3c6d0a6ec78c2f74a0a9
Bean-jun <1342104001@qq.com>
2022-10-23 09:21:56 +0800

    调整页面布局

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,37 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">
         <RouterLink to="/">一个简单的ToDoList</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView />
+      </el-header>
+      <el-main>
+        <RouterView />
+      </el-main>
+      <el-footer class="footer">页脚</el-footer>
+    </el-container>
+  </div>
 </template>
 
+
 <style scoped>
-nav{
-  position: absolute;
-  line-height: 30px;
-  background-color: rgb(0, 0, 0);
-  top: 0px;
-  left: 0px;
-  right: 0px;
+
+.el-container{
+  min-height: 100vh;
 }
-nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
-nav > a{
-  width: 33%;
-  color: aliceblue;
+
+.header{
   text-align: center;
+  line-height: 50px;
+  height: 50px;
+  background-color: #409EFF;
+}
+
+.footer{
+  text-align: center;
+  line-height: 50px;
+  height: 50px;
+  background-color: #409EFF;
 }
 </style>
